f447afee0d7de8bb6e2de069bc2ca86448d75ef5
Ben Millwood <bmillwood@janestreet.com>
2015-05-08 14:13:03 +0100

    Changes to directory structure

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -5,7 +5,7 @@ let dispatch = function
   | Before_options ->
     Options.make_links := false
   | After_rules ->
-    let tag = "pa_typerep" and file = "syntax/with_typerep/pa_typerep_conv.cmo" in
+    let tag = "pa_typerep" and file = "syntax/pa_typerep_conv.cmo" in
     flag ["ocaml"; "compile"; tag] & S[A"-ppopt"; A file];
     flag ["ocaml"; "ocamldep"; tag] & S[A"-ppopt"; A file];
     flag ["ocaml"; "doc"; tag] & S[A"-ppopt"; A file];
