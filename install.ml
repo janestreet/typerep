e56dd86682fd54ea66c4a47a2d1b8b8f0cf5985d
Jeremie Dimino <jdimino@janestreet.com>
2016-12-15 11:20:46 +0000

    114.20+69

--- install.ml
+++
@@ -1,9 +0,0 @@
-#use "topfind";;
-#require "js-build-tools.oasis2opam_install";;
-
-open Oasis2opam_install;;
-
-generate ~package:"typerep"
-  [ oasis_lib "typerep_lib"
-  ; file "META" ~section:"lib"
-  ]
